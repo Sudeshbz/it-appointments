7da27b3e23d77ce5762ea5a21d8b8c1454869f8a
RUKİYE TAPAN <rukiyetapan@RUKIYE-MacBook-Pro.local>
2025-12-23 20:56:07 +0300

    .github

---
+++ tests/test_main.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+from app.main import app 
+import pytest
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_get_services():
+    response = client.get("/services")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+
+def test_create_appointment_unauthorized():
+    response = client.post("/appointments/", json={"notes": "Test randevu"})
+    assert response.status_code == 401  
